4995b9092b9ac831b8248001049f57795c142e3e
Samuel Molinari <samuel@molinari.me>
2021-03-21 11:44:38 +0000

    Support nested css

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const jekyllEnv = process.env.JEKYLL_ENV || "development";
 
 module.exports = {
   plugins: [
+    require("postcss-nested"),
     require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
